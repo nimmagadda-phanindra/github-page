56dd76450402e7764d19f1bc1d8540944e2e7782
Phanindra Nimmagadda <phanindra6121@gmail.com>
2024-11-09 22:01:04 +0530

    Update test_job.sh

--- test_job.sh
+++ test_job.sh
@@ -10,5 +10,5 @@ LOG_FILE="$BUILD_DIR/test_log.txt"
 
 # Step 2: Setup Environment
 echo "🛠 Setting up environment for $PROJECT_NAME..."
-mkdir -p "$BUILD_DIR"
+#mkdir -p "$BUILD_DIR"
 echo "✅ Environment setup complete."
